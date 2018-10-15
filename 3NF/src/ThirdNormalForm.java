a0f3b5f1ece9a9acef0acb7c19e04a714af48a25
Conover Wang <kilaplaya@gmail.com>
2018-10-15 14:31:44 -0700

    step 2

--- 3NF/src/ThirdNormalForm.java
+++ 3NF/src/ThirdNormalForm.java
@@ -1,179 +1,198 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
 
 public class ThirdNormalForm {
-	
-	//hashmap arraylist ints --> arraylist ints
-	//return list of lists
-	
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		File file = new File("input.txt");
-		try {
-			scanner = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found");
-		}
-		HashMap<ArrayList<Integer>, ArrayList<Integer>> FDs = new HashMap<>();
-		ArrayList<Integer> attributes = new ArrayList<>();
-		storeFDs(scanner, FDs, attributes);
-		
-		System.out.println("stored");
-		
-		for (Map.Entry<ArrayList<Integer>, ArrayList<Integer>> entry : FDs.entrySet()) {
-            List<Integer> key = entry.getKey();
-            List<Integer> value = entry.getValue();
-            System.out.println(key + " " + value);
-        }
-		
-		//trying to test closure()
-		ArrayList<Integer> first = new ArrayList<>();
-		first.add(1);
-		first.add(2);
-		
-		ArrayList<Integer> firstRHS = FDs.get(first);
-		System.out.println("firstRHS: " + firstRHS);
-		ArrayList<Integer> c = closure (FDs, first);
-		
-		System.out.println("here goes nothing: ");
-		for (int i = 0; i < c.size(); i++) {
-			System.out.println(c.get(i));
-		}
-		
-//		for (ArrayList<Integer> i : FDs.keySet()) {
-//			for (int j = 0; j < i.size(); j++) {
-//				System.out.print(i.get(j));
-//			}
-//			System.out.println();
-//		}
-		System.out.println("here wweriwer");
-		for (Map.Entry<ArrayList<Integer>, ArrayList<Integer>> entry : FDs.entrySet()) {
-            List<Integer> key = entry.getKey();
-            List<Integer> value = entry.getValue();
-            System.out.println(key + " " + value);
-        }
-	}
-	
-	public static void storeFDs(Scanner scanner, HashMap<ArrayList<Integer>, ArrayList<Integer>> fds, ArrayList<Integer> attributes) {
-		int largestAttribute = 0;
-		while (scanner.hasNextLine()) {
-			String[] data = scanner.nextLine().split(";");
-			String[] lhs = data[0].split(",");
-			String[] rhs = data[1].split(",");
-			ArrayList<Integer> lhsList = new ArrayList<>();
-			ArrayList<Integer> rhsList = new ArrayList<>();
-			for (int i = 0; i < lhs.length; i++) {
-				if (Integer.parseInt(lhs[i]) > largestAttribute) {
-					largestAttribute = Integer.parseInt(lhs[i]);
-				}
-				lhsList.add(Integer.parseInt(lhs[i]));
-			}
-			for (int i = 0; i < rhs.length; i++) {
-				if (Integer.parseInt(rhs[i]) > largestAttribute) {
-					largestAttribute = Integer.parseInt(rhs[i]);
-				}
-				rhsList.add(Integer.parseInt(rhs[i]));
-			}
-			fds.put(lhsList, rhsList);
-		}
-		// A1, ..., An
-		for (int i = 1; i < largestAttribute + 1; i++) {
-			attributes.add(i);
-		}
-	}
-	
-	//pass fds and lhs of what you want to find the closure of
-	//need to account for A --> B and B --> C then C is in the closure of A 
-	
-	public static ArrayList<Integer> closure(HashMap<ArrayList<Integer>, ArrayList<Integer>> fds, ArrayList<Integer> toFindClosureOf) {
-		ArrayList<Integer> x = toFindClosureOf;
-		ArrayList<Integer> xOld = new ArrayList<>();
-		
-		//need to apply splitting rule!
-		//how to account for fds that are like A --> BC (splitting rule)
-		
-		while (!isTwoArrayListsWithSameValues(x, xOld)) {
-			System.out.println("here we are: " + x);
-			xOld = x;
-			
-			for (Map.Entry<ArrayList<Integer>, ArrayList<Integer>> entry : fds.entrySet()) {
-				System.out.println("ok");
-				ArrayList<Integer> key = entry.getKey();
-		        ArrayList<Integer> value = entry.getValue();
-		        if (doesItContain(x, key)) {		//if the lhs of this fd is in x, add the rhs to it (account removing duplicates ex if )
-		        		System.out.println("yip it contains " + key);
-		        		for (int i = 0; i < value.size(); i++) {
-		        			System.out.println("adding");
-		        			ArrayList<Integer> valueToCheck = new ArrayList<>();
-		        			valueToCheck.add(value.get(i));
-		        			System.out.println("is this in x " + value.get(i));
-		        			System.out.println(x);
-		        			if (doesItContain(x, valueToCheck) == false) {	//trying to account for duplicates 
-		        				System.out.println("here");
-		        				x.add(value.get(i));
-		        			}
-		        	  	}
-		        }
-			} 
-			
-		}
-		return x;		
-	}
-	
-	//checks if two (which should be only one element after splitting rule) is in one
-	public static boolean doesItContain(ArrayList<Integer> one, ArrayList<Integer> two){
-		for (int i = 0; i < one.size(); i++) {
-			if (one.get(i) == two.get(0)) return true;
-		}
-		return false;
-	}
-	
-	//checks equality of two ALs
-	public static boolean isTwoArrayListsWithSameValues(ArrayList<Integer> x, ArrayList<Integer> xOld)
-    {
-        //null checking
-        if(x==null && xOld==null)
-            return true;
-        if((x == null && xOld != null) || (x != null && xOld == null))
-            return false;
 
-        if(x.size()!=xOld.size())
-            return false;
-        for(Object itemList1: x)
-        {
-            if(!xOld.contains(itemList1))
-                return false;
-        }
+    // this removes the trivial dependencies as well
+    private static List<String> getFDList(File file) throws FileNotFoundException {
+        Scanner scanner= new Scanner(file);
+        List<String> list = new ArrayList<String>();
+        while (scanner.hasNextLine()) {
+            String[] split = scanner.nextLine().split(";");
+            String result = split[1];
+            for (char c: split[0].toCharArray()) {
+                if (c != ',') {
+                    String replace = "" + c;
 
-        return true;
+                    result = result.replaceAll(replace, "");
+                }
+            }
+            if (!result.equals("")) {
+                list.add(split[0] + ";" + result);
+            }
+
+        }
+        return list;
     }
 
-	// returns a set of all subsets of the attributes of R
-	// you need this method to compute the keys of R (later part of the algorithm)
-	public static ArrayList<Set<Integer>> subsets(Set<Integer> attributes) {
-	    ArrayList<Set<Integer>> sets = new ArrayList<Set<Integer>>();
-	    if (attributes.isEmpty()) {
-	        sets.add(new HashSet<Integer>());
-	        return sets;
-	    }
-	    ArrayList<Integer> list = new ArrayList<Integer>(attributes);
-	    int head = list.get(0);
-	    Set<Integer> rest = new HashSet<Integer>(list.subList(1, list.size())); 
-	    for (Set<Integer> set : subsets(rest)) {
-	        Set<Integer> newSet = new HashSet<Integer>();
-	        newSet.add(head);
-	        newSet.addAll(set);
-	        sets.add(newSet);
-	        sets.add(set);
-	    }       
-	    return sets;
-	}
+    private static List<String>  singletonRHS(List<String> fds) {
+        List<String> result = new ArrayList<String>();
+        for (String fd: fds) {
+            String[] split = fd.split(";");
+            if (split[1].length() > 1 ) {
+                for (int i = 0; i < split[1].length(); i ++) {
+                    if (!split[1].substring(i, i+1).equals(",")) {
+                        String temp = split[0] + ";" + split[1].substring(i, i + 1);
+                        result.add(temp);
+                    }
+                }
+            } else {
+                result.add(fd);
+            }
+        }
 
-	// find the superkeys by computing the closure of all the subsets of the attributes
-	// the subsets whose closures are equal to all the attributes are superkeys
-	public static HashMap<ArrayList<Integer>, ArrayList<Integer>> findSuperkeys(
-			HashMap<ArrayList<Integer>, ArrayList<Integer>> fds, ArrayList<Integer> attributes) {
-		return null;
-	}
+        return result;
+    }
+
+    private static List<String> reduce(List<String> fds)  {
+        List<String> result = new ArrayList<String>();
+        for (String fd: fds) {
+            List<String> subset = new ArrayList<String>();
+            subset.addAll(fds);
+            subset.remove(fd);
+
+            String[] split = fd.split(";");
+            HashSet<String> closure = closure(split[0], subset);
+            if (!closure.contains(split[1])) {
+                result.add(fd);
+            }
+
+        }
+        return result;
+    }
+    private static List<String> minimizeLHS(List<String> fds ) {
+        List<String> result = new ArrayList<String>();
+        for( String fd: fds) {
+            String[] split = fd.split(";");
+            if (split[0].length() > 1) {
+                result.add(removeLHS(split[0], fds) + ";" + split[1]);
+            } else {
+                result.add(fd);
+            }
+        }
+        return result;
+    }
+
+    private static String removeLHS(String leftFD, List<String> fds) {
+        String result = leftFD;
+        for( int i = 0; i < leftFD.length(); i++) {
+            String current = leftFD.substring(i, i+1);
+            HashSet<String> closure = closure(current, fds);
+            for (String key:  closure) {
+                if ( leftFD.contains(key)  && !key.equals(current)) {
+                    result = result.replaceAll(key, "");
+                    result = result.replace(",", "");
+                }
+            }
+        }
+        return result;
+    }
+
+    private static List<String> listifyString(String string) {
+        List<String> result = new ArrayList<String>();
+        for (int i = 0; i < string.length(); i++) {
+            if(!string.substring(i, i+1).equals(","))
+                result.add(string.substring(i, i+1));
+        }
+        return result;
+    }
+    private static HashSet<String> closure(String target, List<String> fds ) {
+        HashSet<String> closure = new HashSet<String>();
+        HashSet<String> xOld = new HashSet<String>();
+        closure.add(target);
+
+        while (!xOld.containsAll(closure)) {
+            xOld = new HashSet<String>(closure);
+            for (String fd: fds) {
+                String[] split = fd.split(";");
+                if (split[0].length() > 1 ) {
+                     if (closure.containsAll(listifyString(split[0]))) {
+                         closure.add(split[1]);
+                     }
+                } else {
+                    if (closure.contains(split[0])) {
+                        closure.add(split[1]);
+                    }
+                }
+            }
+        }
+
+
+        return closure;
+    }
+
+    private static List<String> minimalBasis(File file) throws FileNotFoundException {
+        return reduce(minimizeLHS(singletonRHS(getFDList(file))));
+    }
+
+    // step 2
+    private static List<String> schematize(List<String> fds) {
+        List<String> result = new ArrayList<String>();
+        for (String fd: fds) {
+            result.add(fd.replaceAll(";", ","));
+        }
+        return clean(result);
+    }
+    private static List<String> clean(List<String> fds) {
+        List<String> result = new ArrayList<String>();
+        HashSet<String> seen = new HashSet<String>();
+        for (String fd: fds) {
+//            System.out.println ("fd: " + fd);
+//            for (String s: seen) {
+//                System.out.println(s);
+//            }
+            boolean flag = true;
+            for (char c: fd.toCharArray()) {
+                if (c != ',') {
+                    if (!seen.add("" + c)) {
+                        flag = false;
+                    } else {
+                        flag = true;
+                    }
+                }
+            }
+            if (flag) {
+                result.add(fd);
+            }
+        }
+        return result;
+    }
+
+    private static HashSet<String> getAttributes(File file) throws FileNotFoundException {
+        HashSet<String> attributes = new HashSet<String>();
+        Scanner scanner= new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine().replaceAll(",", "").replaceAll(";","");
+            for (char c: line.toCharArray()) {
+                attributes.add(""+ c);
+            }
+        }
+        return attributes;
+    }
+    public static void main (String[] args) throws FileNotFoundException {
+        File file = new File("input.txt");
+
+//        for (String s: attributes) {
+//            System.out.println(s);
+//        }
+
+        // Find a minimal basis for F, say G.
+        // For each FD X→A in G, use XA as the schema of one of the relations in the decomposition.
+        List<String> fds = schematize(minimalBasis(file));
+        for (String s: fds) {
+            System.out.println(s);
+        }
+
+        // If none of the relation schema from Step 2 is a superkey for R, add another relation whose schema is a key for R.
+        HashSet<String> attributes = getAttributes(file);
+
+//        List<String> fds = getFDList(file);
+//        for (String s: fds) {
+//            System.out.println(s);
+//        }
+    }
 }
